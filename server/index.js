cc1085d7d95b71262ab0a9421e3fe4769ee9042b
Frankie2Fast <Frankiepc3@gmail.com>
2026-03-14 09:41:14 -0400

    Auto-save: 2026-03-14 13:41

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ import { existsSync } from 'fs';
 
 import authRoutes from './routes/auth.js';
 import tradeRoutes from './routes/trades.js';
-import uploadRoutes from './routes/uploads.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
